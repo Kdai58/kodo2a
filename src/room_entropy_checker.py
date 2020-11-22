fa64d2423e14b6c5c2a5ef57d14312b33762fdaf
KanamoriMitsutaka <mitchy5551.sharuru@gmail.com>
2020-11-22 22:08:38 +0900

    もう一度送ります

---
+++ src/room_entropy_checker.py
@@ -0,0 +1,11 @@
+#kanamori
+#2020.11.22
+#ver1.0
+#書きはじめ
+
+class RoomEntropyChecker:
+    __SLEEP_SEC:Integer = 5000
+    def loop_gui(self):
+
+gui_manager = GUIManager()
+gui_manager.update_gui()
