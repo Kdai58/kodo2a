a9238da9f1cf3ee3efbb9a237b280deb9f126e5a
Kdai58 <khydrm0508@gmail.com>
2020-11-29 23:48:10 +0900

    CameraImgExtractorを修正．（デバッグ待ち）#47

--- src/camera_img_extractor.py
+++ src/camera_img_extractor.py
@@ -23,10 +23,10 @@ class CameraImgExtractor:
 
     def __init__(self):
         self.cap = None
-        self._WEB_CAMERA_NUMBER = 1
+        self._WEB_CAMERA_NUMBER = 0
         self._REDUCTION_RATIO = 2
-        self._open_webcam_stream()
         self.is_exist_webcam = False
+        self._open_webcam_stream()
 
     def _open_webcam_stream(self):
         # self.cap = cv2.VideoCapture(self._WEB_CAMERA_NUMBER)
@@ -38,20 +38,23 @@ class CameraImgExtractor:
 
 
     def exists_webcam(self):
-        # while(True):
-        #     ret, frame = self.cap.read()
-        #     if ret == False:
-        #         print("fialed to open webcam")
-        #         break
-        # else:
         return self.is_exist_webcam
 
     def read_img(self):
-        try:
-            ret, frame = self.cap.read()
-            self.is_exist_webcam = True
-        except cv2.error:
+        # try:
+        #     ret, frame = self.cap.read()
+        #     self.is_exist_webcam = True
+        # except cv2.error:
+        #     self.is_exist_webcam = False
+        # frame = cv2.resize(frame, (frame.shape[1] // self._REDUCTION_RATIO, frame.shape[0] // self._REDUCTION_RATIO))
+        # return frame
+
+        ret, frame = self.cap.read()
+
+        if (ret != True):
             self.is_exist_webcam = False
+            return np.zeros((100, 100))
+
         frame = cv2.resize(frame, (frame.shape[1] // self._REDUCTION_RATIO, frame.shape[0] // self._REDUCTION_RATIO))
         return frame
 
@@ -63,12 +66,16 @@ if __name__ == "__main__":
     camera_img_extractor = CameraImgExtractor()
     print(f"open webcam stream:{camera_img_extractor.is_exist_webcam}")
 
+    i = 0
     while 1:
-        ret, frame = camera_img_extractor.cap.read()
-        print(f"read image:{camera_img_extractor.is_exist_webcam}")
+        frame = camera_img_extractor.read_img()
+        print(f"{i}:read image:{camera_img_extractor.is_exist_webcam}")
         cv2.imshow('sample', frame)
 
         k = cv2.waitKey(1)
         if k == 27:
             break
+
+        i += 1
+        time.sleep(3)
     camera_img_extractor.release_webcam_stream()
